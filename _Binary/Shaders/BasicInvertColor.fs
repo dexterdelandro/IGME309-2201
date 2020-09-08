4e2ea9dbe68d67ecfe05b3954f9e547fe31e5488
Dexter Delandro <dexter.delandro@apater.com>
2020-09-07 22:54:53 -0400

    Created and Completed E03

--- _Binary/Shaders/BasicInvertColor.fs
+++ _Binary/Shaders/BasicInvertColor.fs
@@ -2,13 +2,17 @@
 
 in vec3 Color;
 
+uniform bool Complimentary;
 uniform vec3 SolidColor = vec3(-1,-1,-1);
 
 out vec4 Fragment;
 
 void main()
 {
-	Fragment = vec4(1-Color.r, 1-Color.g, 1-Color.b,1);
+	if(Complimentary)
+		Fragment = vec4(1.0 - Color.r, 1.0 - Color.g, 1.0 - Color.b,1);
+	else
+		Fragment = vec4(Color.r, Color.g, Color.b, 1);
 	
 	if(SolidColor.r != -1.0 && SolidColor.g != -1.0 && SolidColor.b != -1.0)
 		Fragment = vec4(SolidColor, 1);
--- include/Simplex/Shaders/BasicInvertColor.vs
+++ include/Simplex/Shaders/BasicInvertColor.vs
@@ -8,7 +8,7 @@ out vec4 Fragment;
 
 void main()
 {
-	Fragment = vec4(1-Color.r, 1-Color.g, 1-Color.b,1);
+	Fragment = vec4(Color,1);
 	
 	if(SolidColor.r != -1.0 && SolidColor.g != -1.0 && SolidColor.b != -1.0)
 		Fragment = vec4(SolidColor, 1);
